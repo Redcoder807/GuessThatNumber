ed157e541fe473611561a7cddba706a956bef490
Redcoder807 <gearonhaynes@gmail.com>
2022-02-28 13:47:42 -0600

    Dunno

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ let attempts;
 let guess;
 // For storing the user's response to play again or not to play again
 let playAgain;
+
+// Starting alert message
+alert(`Welcome to "GUESS THAT NUMBER!" Please click "OK" to start the game.`)
+
+// Game restarts as long as restartGame has a value of true
+while (restartGame){
+    // Asks user to enter a number to set the upper bound for the random number that will be created.
+    rangeNum = prompt(`Please enter a maximum number for the range`)
+
+    // Using parseInt to attempt to convert the user's response into a number value
+    rangeNum = parseInt(rangeNum)
+
+    // verifies thw user's entry for a range number is a number greater than zero (NOTE: All number, positive or negative, have default boolean value of true, except for zero which has a default booleans value of false. ALso, NaN has a default boolean value of false as well)
+    while (!rangeNum || rangeNum < 1){
+        rangeNum = prompt(enterNumText);
+        rangeNum = parseInt(rangeNum);
+    }
+
+    // Creates the random number using the range number entered by the user
+    randomNum = Math.floor(Math.random() * rangeNum + 1)
+
+    break;
+}
